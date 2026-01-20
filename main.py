fd2bd04d21b041313d5b43a394d0935a662752f4
GloriaSibaja <gsibaja@lasallistas.org.mx>
2026-01-19 20:33:27 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import Saludo-Cobos
+import Saludo-Dali
+
+if __name__ == "__main__":
+    print("=" * 40)
+    print("--- INICIANDO INTEGRACIÓN DEVOPS ---")
+    print("=" * 40)
+    
+    # Invocación de las funciones de cada módulo
+    Saludo-Cobos.saludo()
+    Saludo-Dali.saludo()
+    
+    print("=" * 40)
+    print("--- DESPLIEGE FINALIZADO ---")
